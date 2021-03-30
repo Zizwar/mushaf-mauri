95d87310ca0f2ab602fcae9caa8998b1cf25af71
Zizwar <zizwar@gmail.com>
2021-03-30 05:35:14 +0000

    creat db conect mysql

---
+++ src/functions/dbs.js
@@ -0,0 +1,37 @@
+import * as SQLite from "expo-sqlite";
+import * as FileSystem from "expo-file-system";
+const folderSqlit = `${FileSystem.documentDirectory}SQLite`;
+const execSql = async (sql, params = [], db) => {
+  return new Promise((resolve, reject) =>
+    db.transaction((tx) => {
+      tx.executeSql(sql, params, (_, { rows }) => resolve(rows._array));
+    })
+  );
+};
+
+
+export class dbs {
+  constructor(author) {
+    this.author = author;
+    this.db = null;
+  }
+  async connect() {
+    let folderSqlitInfo = await FileSystem.getInfoAsync(folderSqlit);
+    console.log({ folderSqlitInfo });
+    listDB = folderSqlitInfo.exists
+      ? await FileSystem.readDirectoryAsync(folderSqlit)
+      : null;
+    console.log({ listDB });
+
+    this.db = listDB.includes(`${this.author}.db`)
+      ? SQLite.openDatabase(`${this.author}.db`)
+      : null;
+  }
+  get ping() {
+    return this.db;
+  }
+
+  async execSql(sql, params = []) {
+    return await execSql(sql, params, this.db);
+  }
+}
--- src/functions/index.js
+++ src/functions/index.js
@@ -1,2 +1,3 @@
 export * from './coordinatePage';
 export * from './helper';
+export * from './db';
