aab8b49c937cf8db292bfd2e86eda784714a0fa6
Zizwar <zizwar@gmail.com>
2021-03-30 11:39:42 +0000

    br909 f rass

--- src/functions/coordinatePage.js
+++ src/functions/coordinatePage.js
@@ -3,23 +3,24 @@ import { Dimensions } from "react-native";
 
 let { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 //Create layer  quran mauri
-const DEVICE_WIDTH_HEIGHT = { width: WIDTH, height: HEIGHT }; //change to dynamic dimension
+const DEVICE_WIDTH_HEIGHT = { width: WIDTH-1, height: HEIGHT }; //change to dynamic dimension
 
 const ORIGINAL_WIDTH_HEIGHT = { width: 456, height: 825 }; //no change /1273
 const WIDTH_SCREEN_RENDER = 456;
 const NEXT_PAGE_LEFT = 0;
 const NEXT_PAGE_TOP = 985.8; // ORIGINAL_WIDTH_HEIGHT.height;
 const MARGIN_LEFT_AYA = 10;
-const MARGIN_TOP = 20;
-const MARGIN_HEIGHT_AYA = -1;
+const MARGIN_PAGE_TOP = 0;
+const MARGIN_HEIGHT_AYA = 0.3;
 const MARGIN_PAGE = 10;
+const MARGIN_HEIGHT_SCALA = 0;
 
 const HEIGHT_SCALA =
-  (NEXT_PAGE_TOP - MARGIN_PAGE) *
-  (WIDTH / ORIGINAL_WIDTH_HEIGHT.width);
+  ((NEXT_PAGE_TOP - MARGIN_PAGE) *
+  (WIDTH / ORIGINAL_WIDTH_HEIGHT.width))+ MARGIN_HEIGHT_SCALA;
 
 const NUMBER_LINE = 15;
-const HEIGHT_LINE = HEIGHT_SCALA / NUMBER_LINE; //+ MARGIN_HEIGHT_AYA;
+const HEIGHT_LINE = HEIGHT_SCALA / NUMBER_LINE + MARGIN_HEIGHT_AYA;
 
 export const coordinatePage = (page, mosshaf) => {
   let oldLine = 0;
@@ -124,7 +125,7 @@ export const coordinatePage = (page, mosshaf) => {
 //
 const renderLineFahres = ({ left, line, width, wino }) => {
   //===>
-  let top = +(HEIGHT_LINE * line).toFixed(2); //+200;
+  let top = +(HEIGHT_LINE * line).toFixed(2) + MARGIN_PAGE_TOP; //+200;
   let height = +HEIGHT_LINE.toFixed(2);
   left = +left.toFixed(2);
   width = +width.toFixed(2);
