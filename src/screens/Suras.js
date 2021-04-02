6d0bebd12cbb7fe2840628e5385329c6268f262f
Zizwar <zizwar@gmail.com>
2021-04-02 06:44:26 +0000

    fix 7izb

--- src/screens/Suras.js
+++ src/screens/Suras.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import {
-  Container,
-  Content,
-  List,
-  ListItem,
-  Text,
-  Body,
-} from "native-base";
+import { Container, Content, List, ListItem, Text, Body } from "native-base";
 import { isRTL } from "expo-localization";
 
 import { Icon } from "../component";
-import { allSuwar, getAllAyaSuraBySura, getJuzBySuraAya,getHizbBySuraAya,getPageBySuraAya } from "../functions";
+import {
+  allSuwar,
+  getAllAyaSuraBySura,
+  getJuzBySuraAya,
+  getHizbBySuraAya,
+  getPageBySuraAya,
+} from "../functions";
 import { connect } from "react-redux";
 import { setExactAya } from "../../reducer";
 import * as lang from "../../i18n";
@@ -23,13 +22,9 @@ class Suras extends Component {
     this.lang = lang[this.props.lang];
   }
   UNSAFE_componentWillMount() {
-    // this.setState({vi: this.getData(NUM_DATA, 0)})
-    // this.setState({vi: [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}, {id: 11}]})
     this.allSuwar = allSuwar(this.props.lang);
   }
-  goBack = () => {
-    this.props.navigation.goBack();
-  };
+  goBack = () => this.props.navigation.goBack();
 
   onPressSura = (id) => {
     this.props.setExactAya({ aya: 1, sura: id });
@@ -42,7 +37,7 @@ class Suras extends Component {
       lang,
     } = this.props;
     return (
-      <Container style={{backgroundColor}}>
+      <Container style={{ backgroundColor }}>
         <Headerino
           onPress={this.goBack}
           lang={lang}
@@ -51,34 +46,44 @@ class Suras extends Component {
           //icon={"ios-menu"}
           backgroundColor={backgroundColor}
         />
-        <Content style={{backgroundColor}}>
+        <Content style={{ backgroundColor }}>
           <List
             dataArray={this.allSuwar}
             renderRow={(data) => (
               <ListItem
-              style={{backgroundColor}}
+                style={{ backgroundColor }}
                 onPress={() => {
                   this.onPressSura(data.id);
                 }}
               >
                 <Body>
-                  <Text style={{color,textAlign:isRTL && lang!=="ar"? "right":null,fontSize:18}}>
-                    {data.id + ". " + this.lang["sura_s"] + " " + data.name}
+                  <Text
+                    style={{
+                      color,
+                      textAlign: isRTL && lang !== "ar" ? "right" : null,
+                      fontSize: 18,
+                    }}
+                  >
+                    {`${data.id}. ${this.lang["sura_s"]} ${data.name}`}
                   </Text>
-                  <Text style={{color,textAlign:isRTL && lang!=="ar"? "right":null}} note>
-                    {getAllAyaSuraBySura(data.id).length +
-                      " " +
-                      this.lang["aya_s"] +
-                      ", " +
-                      this.lang["juz"] +
-                      " " +
-                      getJuzBySuraAya({ sura: data.id, aya: 1 })+  ", " +
-                      this.lang["hizb"] +
-                      " " +
-                      getHizbBySuraAya({ sura: data.id, aya: 1 }) + ", " +
-                      this.lang["page"] +
-                      " " +
-                      getPageBySuraAya({ sura: data.id, aya: 1 })}
+                  <Text
+                    style={{
+                      color,
+                      textAlign: isRTL && lang !== "ar" ? "right" : null,
+                    }}
+                    note
+                  >
+                    {`${getAllAyaSuraBySura(data.id).length} ${
+                      this.lang["aya_s"]
+                    }, ${this.lang["juz"]} ${parseInt(
+                      getJuzBySuraAya({ sura: data.id, aya: 1 }) / 4
+                    )}, ${this.lang["hizb"]} ${getHizbBySuraAya({
+                      sura: data.id,
+                      aya: 1,
+                    })}, ${this.lang["page"]} ${getPageBySuraAya({
+                      sura: data.id,
+                      aya: 1,
+                    })}`}
                   </Text>
                 </Body>
               </ListItem>
