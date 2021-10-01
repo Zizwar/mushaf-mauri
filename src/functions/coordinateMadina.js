6cc8b65485f30c4fd4f9a95ebbaf61705092a612
Zizwar <zizwar@gmail.com>
2021-10-01 03:24:04 +0000

    create func coordinate warsh madina

---
+++ src/functions/coordinateMadina.js
@@ -0,0 +1,169 @@
+import { Dimensions } from "react-native";
+import { coordinateMadina } from "../data";
+const { width: WIDTH } = Dimensions.get("window");
+let scrW_mosshaf = WIDTH + 20;
+
+export const coordinatePageMadina = (page) => {
+
+  const coordinatePage = coordinateMadina[page];
+  let sura,
+    aya,
+    top,
+    left,
+    prev_top,
+    prev_left,
+    width,
+    diff,
+    hl_id,
+    b_top = 0,
+    b_left = 0,
+    //
+    height = 30,
+    mgwidth = 40,
+    twidth = 416,
+    ofwidth = 10,
+    ofheight = 15,
+    fasel_sura = 110,
+    page_top = 37,
+    page_sura_top = 80,
+    fp_height = 20,
+    fp_mgwidth = 80,
+    fp_twidth = 376,
+    fp_ofwidth = 5,
+    fp_ofheight = 10;
+  //
+
+  if (page === 1 || page === 2) {
+    height = fp_height;
+    mgwidth = fp_mgwidth;
+    twidth = fp_twidth;
+    ofwidth = fp_ofwidth;
+    ofheight = fp_ofheight;
+  }
+  prev_top = null;
+  prev_left = null;
+  let count = 1;
+  let allPosition = [];
+  for (let [sura, aya, left, top] of coordinatePage) {
+    top = top - ofheight;
+    left = left - ofwidth;
+    let wino_ = {
+      aya: aya,
+      sura: sura,
+      page: page,
+      id: "s" + sura + "a" + aya + "z",
+    };
+    width = 0;
+
+    hl_id = "s" + sura + "a" + aya + "z";
+    if (count === 1) {
+      prev_left = twidth;
+      if (page === 1 || page === 2) {
+        prev_top = 270;
+      } else {
+        if (aya === 1) {
+          prev_top = page_sura_top;
+        } else {
+          prev_top = page_top;
+        }
+      }
+    } else {
+      if (aya === 1) {
+        prev_top += fasel_sura;
+        prev_left = twidth;
+      }
+    }
+    diff = top - prev_top;
+    if (diff > height * 1.6) {
+      allPosition.push(
+        hl_draw(
+          hl_id + "_1",
+          prev_top,
+          mgwidth,
+          prev_left - mgwidth,
+          height,
+          b_top,
+          b_left,
+          wino_ //.id+"d_1"
+        )
+      );
+      allPosition.push(
+        hl_draw(
+          hl_id + "_2",
+          top,
+          left,
+          twidth - left,
+          height,
+          b_top,
+          b_left,
+          wino_ //_.id+ "_2"
+        )
+      );
+      allPosition.push(
+        hl_draw(
+          hl_id + "_3",
+          prev_top + height,
+          mgwidth,
+          twidth - mgwidth,
+          diff - height,
+          b_top,
+          b_left,
+          wino_ //id +"_3"
+        )
+      );
+    } else if (diff > height * 0.6) {
+      allPosition.push(
+        hl_draw(
+          hl_id + "_1",
+          prev_top,
+          mgwidth,
+          prev_left - mgwidth,
+          height,
+          b_top,
+          b_left,
+          wino_ //.id+"d_1"
+        )
+      );
+      allPosition.push(
+        hl_draw(
+          hl_id + "_2",
+          top,
+          left,
+          twidth - left,
+          height,
+          b_top,
+          b_left,
+          wino_ //.id +"_2"
+        )
+      );
+    } else {
+      width = prev_left - left;
+      allPosition.push(
+        hl_draw(hl_id + "_1", top, left, width, height, b_top, b_left, wino_)
+      );
+    }
+    count++;
+    prev_top = top;
+    prev_left = left;
+  }
+  return allPosition;
+
+};
+//
+
+function hl_draw(id, top, left, width, height, b_top = 0, b_left = 0, wino) {
+  //  "agr b_top =",b_top,"_______________________");
+  //let b_top = 0;
+  //let b_left = 0;
+  //top = b_top + top;
+  //left = b_left + left;
+  scrW_mosshaf = scrW_mosshaf + 300;
+  width = (scrW_mosshaf / 456) * width;
+  height = (scrW_mosshaf / 456) * height;
+  left = (scrW_mosshaf / 456) * left; //+ 2;
+  top = (scrW_mosshaf / 456) * top; //+ 7;
+
+  return { width, height, left, top, wino, id };
+
+}
+//
---
+++ src/functions/coordinateMuhammadi.js
@@ -0,0 +1,138 @@
+import { coordinateMuhammadi } from "../data";
+import { Dimensions } from "react-native";
+
+const { width: WIDTH } = Dimensions.get("window");
+//Create layer  quran mauri
+//change to dynamic dimension
+ 
+const NEXT_PAGE_LEFT = 0;
+
+const MARGIN_LEFT_AYA = 15;
+const MARGIN_PAGE_TOP = 4.2;
+const MARGIN_HEIGHT_AYA = 0.3;
+const MARGIN_WIDTH = 25;
+const ORIGIN_PAGE_TOP = 985.8;
+const ORIGINAL_WIDTH_HEIGHT = {
+  width: 456,
+  height: ORIGIN_PAGE_TOP + MARGIN_PAGE_TOP,
+};
+/*
+  ((ORIGIN_PAGE_TOP - MARGIN_PAGE) *
+  (WIDTH / ORIGINAL_WIDTH_HEIGHT.width))+ MARGIN_HEIGHT_SCALA;
+  */
+const HEIGHT_SCALA = WIDTH * 1.471676300578035 - MARGIN_WIDTH;
+const DEVICE_WIDTH_HEIGHT = { width: WIDTH, height: HEIGHT_SCALA };
+const NUMBER_LINE = 15;
+const HEIGHT_LINE = HEIGHT_SCALA / NUMBER_LINE + MARGIN_HEIGHT_AYA;
+
+export const coordinatePageMuhammadi = (page) => {
+  let oldLine = 0;
+  let oldLeft = DEVICE_WIDTH_HEIGHT.width;
+
+  const allPosition = [];
+  //===>
+  const [nextPageSura = null, nextAya] = coordinateMuhammadi[page + 1]
+    ? coordinateMuhammadi[page + 1][0]
+    : [];
+
+  const coordinatePage = nextPageSura
+    ? [
+        ...coordinateMuhammadi[page],
+        [nextPageSura, nextAya, NEXT_PAGE_LEFT, ORIGIN_PAGE_TOP],
+      ]
+    : coordinateMuhammadi[page];
+
+  for (let [sura, aya, left, top] of coordinatePage) {
+    left =
+      (left / ORIGINAL_WIDTH_HEIGHT.width) * DEVICE_WIDTH_HEIGHT.width -
+      MARGIN_LEFT_AYA;
+    let line = parseInt(
+      ((top / ORIGINAL_WIDTH_HEIGHT.height) * DEVICE_WIDTH_HEIGHT.height) /
+        HEIGHT_LINE
+    );
+
+    //if 1 line
+    const lineNumber = line - oldLine;
+    thisLine = line;
+    const wino = {
+      aya,
+      sura,
+      page,
+      id: `s${sura}a${aya}z`,
+    };
+    if (!lineNumber) {
+      //alert("lineNumber1")
+      allPosition.push(
+        renderLineFahres({ left, line, width: oldLeft - left, wino })
+      );
+      // oldLeft = left;
+      //  return;
+    }
+    //if 2 lineNumber
+    else if (lineNumber === 1) {
+      //alert("lineNumber2")
+      //lastLine
+      allPosition.push(
+        renderLineFahres({
+          left,
+          line,
+          width: DEVICE_WIDTH_HEIGHT.width - left,
+          wino,
+        })
+      );
+      //firstLine
+      allPosition.push(
+        renderLineFahres({ left: 0, line: oldLine, width: oldLeft, wino })
+      );
+      // oldLine = thisLine;
+      // oldLeft = left;
+    }
+    //if multilineNumber
+    else if (lineNumber > 1) {
+      //alert("multi line")
+      //lastLine
+      allPosition.push(
+        renderLineFahres({
+          left,
+          line,
+          width: DEVICE_WIDTH_HEIGHT.width - left,
+          wino,
+        })
+      );
+      //firstLine
+      allPosition.push(
+        renderLineFahres({ left: 0, line: oldLine, width: oldLeft, wino })
+      );
+
+      //renderMultiLine Btwn
+      for (let ii = oldLine + 1; ii < thisLine; ii++)
+        allPosition.push(
+          renderLineFahres({
+            left: 0,
+            line: ii,
+            width: DEVICE_WIDTH_HEIGHT.width,
+            wino,
+          })
+        );
+    }
+    oldLine = thisLine;
+    oldLeft = left;
+  }
+
+  return allPosition;
+  ////===>
+};
+//
+const renderLineFahres = ({ left, line, width, wino }) => {
+  //===>
+  let top = +(HEIGHT_LINE * line).toFixed(2) + MARGIN_PAGE_TOP; //+200;
+  let height = +HEIGHT_LINE.toFixed(2);
+  left = +left.toFixed(2);
+  width = +width.toFixed(2);
+  //recalc
+
+  //  console.log("original", { left, line, width, top, height, id: wino.id });
+  return { left, width, top, height, id: wino.id, wino };
+};
+
+//
